245f37ecbe60abead4cb039b691c85cd06873ef8
Kevin Chuang <kcxinkun@gmail.com>
2015-11-15 10:17:34 -0800

    css file init commit, index add css link, script add element selectors to change answer colors

--- game.js
+++ game.js
@@ -86,12 +86,18 @@ w=question(questionOne,"yes",'qOne','imgOne');
 if(w===true){
   document.getElementById('qOne').innerHTML= "That's right " + name + "!" + " " + "I love to play video games!";
   document.getElementById('imgOne').src= "img/check.jpg";
+  document.getElementById('qOne').className = "correct";
   a=1;
 }
 else if(w===false){
   document.getElementById('qOne').innerHTML="Sorry " + name + ", that's incorrect. I play games very often.";
   document.getElementById('imgOne').src= "img/cross.jpg";
+  document.getElementById('qOne').className = "incorrect";
+}
 
+else {
+ document.getElementById('imgOne').src= "img/cross.jpg";
+ document.getElementById('qOne').className = "incorrect";
 }
 
 //QUESTION 2
@@ -100,12 +106,18 @@ w=question(questionTwo,"yes",'qTwo');
 if(w===true){
 	document.getElementById('qTwo').innerHTML="That's right " + name + "!" + " " + "I am a vampire and work night shift!";
   document.getElementById('imgTwo').src= "img/check.jpg";
+  document.getElementById('qTwo').className = "correct";
   b=1;
 }
 else if(w===false){
 	document.getElementById('qTwo').innerHTML="Sorry " + name + ", I wish you were correct. Unfortunately I work night shift.";
   document.getElementById('imgTwo').src= "img/cross.jpg";
+  document.getElementById('qTwo').className = "incorrect";
+}
 
+else {
+ document.getElementById('imgTwo').src= "img/cross.jpg";
+ document.getElementById('qTwo').className = "incorrect";
 }
 
 //QUESTION 3
@@ -114,12 +126,18 @@ w=question(questionThree,"no",'qThree','imgOne');
 if(w===true){
   document.getElementById('qThree').innerHTML="That's right " + name + "!" + " " + "I was from Burlington, North Carolina before I came to Seattle.";
   document.getElementById('imgThree').src= "img/check.jpg";
+  document.getElementById('qThree').className = "correct";
   c=1;
 }
 else if(w===false){
   document.getElementById('qThree').innerHTML="Sorry " + name + ". " +"I am not originally from Seattle, Washington."
   document.getElementById('imgThree').src= "img/cross.jpg";
+  document.getElementById('qThree').className = "incorrect";
+}
 
+else {
+ document.getElementById('imgThree').src= "img/cross.jpg";
+ document.getElementById('qThree').className = "incorrect";
 }
 
 //QUESTION 4
@@ -129,11 +147,13 @@ w=question(questionFour,25,'qFour');
 if(w === true){
   document.getElementById('qFour').innerHTML="That's right " + name + "!" + " " + "I am 25 years old!";
   document.getElementById('imgFour').src= "img/check.jpg";
+  document.getElementById('qFour').className = "correct";
   d = 1;
 }
 
 else {
  document.getElementById('imgFour').src= "img/cross.jpg";
+ document.getElementById('qFour').className = "incorrect";
 }
 
 //QUESTION 5
@@ -142,11 +162,17 @@ w=question(questionFive,"no",'qFive');
 if(w===true){
   document.getElementById('qFive').innerHTML="That's right " + name + "!" + " " + "I like licorice type candy, but I'm not fond of the true licorice flavor.";
   document.getElementById('imgFive').src= "img/check.jpg";
+  document.getElementById('qFive').className = "correct";
   e=1;
 }
 else if(w===false){
   document.getElementById('qFive').innerHTML="Sorry " + name + ". " +"I don't prefer the licorice flavor."
   document.getElementById('imgFive').src= "img/cross.jpg";
+  document.getElementById('qFive').className = "incorrect";
+}
+else {
+ document.getElementById('imgFive').src= "img/cross.jpg";
+ document.getElementById('qFive').className = "incorrect";
 }
 
 //QUESTION 6
@@ -155,10 +181,12 @@ w=question(questionSix,15,'qSix');
 if(w === true){
   document.getElementById('qSix').innerHTML="That's right " + name + "!" + " " + "I was born on the Ides of March!";
   document.getElementById('imgSix').src= "img/check.jpg";
+  document.getElementById('qSix').className = "correct";
   f = 1;
 }
 else {
   document.getElementById('imgSix').src= "img/cross.jpg";
+  document.getElementById('qSix').className = "incorrect";
 }
 
 function total(a,b,c,d,e,f){
